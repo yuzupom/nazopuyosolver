010fca3e7ef770e23864488113c1062d7c69f6d9
K A <k_asano@K-no-MacBook-Air.local>
2015-05-18 15:08:20 +0900

    60回ループを適切な回数ループするように変更

--- nazopuyosolver.js
+++ nazopuyosolver.js
@@ -1,3 +1,4 @@
+// 2手で1秒
 var gol_str = gol[0].split(",")
 var tsumo_num = gol_str.length
 var gogen = []
@@ -10,33 +11,47 @@ var test_go = new Array(tsumo_num)
 bruteforce_search()
 
 function bruteforce_search(){
+	var time = Date.now()
 	var current_puyofu = new Array(tsumo_num);
+	var chkstr;
+	past_field_str = null,new_field_str = null;
 	for(var i = 0;i < tsumo_num; i++){
 		current_puyofu[i] = 0;
 	}
-	while(true){
+	while(current_puyofu != null){
 		for (var i = 0; i < tsumo_num; i++){
 			test_go[i] = gogen[i] + TSUMO_PATTERN[current_puyofu[i]]
 		}
 		faqmf()
 		go = test_go
-		for (var i = 0; i < 60;i++){
+		while ( !(goi == tsumo_num - 1 && gok == false) ){
 			faqmn()
 		}
-		var chkstr = lbI.innerHTML.toUpperCase()
+		new_field_str = gf.toString()
+		do {
+			past_field_str = new_field_str
+			faqmn()
+			new_field_str = gf.toString()
+		}while(gok == true && past_field_str != new_field_str);
+		chkstr = lbI.innerHTML.toUpperCase()
 		if( chkstr.indexOf('<B><FONT COLOR="#FF0000">') != -1){
-			alert(test_go)
+			//alert(test_go)
 			break;
 		}
 
 		current_puyofu = next_puyofu(current_puyofu)
 	}
+	alert(Date.now() - time)
+	alert(test_go)
 }
 
 function next_puyofu(puyofu){
 	for(var i = 0;i < puyofu.length;i++){
 		puyofu[i] ++;
 		if(puyofu[i] >= TSUMO_PATTERN.length){
+			if(i == puyofu.length - 1){
+				return null;
+			}
 			puyofu[i] = 0;
 		}else{
 			break;
